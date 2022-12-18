4910bbb39ecc2dc17457d604db92bb0711145058
Manoa Aro <hei.manoa.2@gmail.com>
2022-12-19 00:14:10 +0100

    feat: add java files

--- V0_1__Initialize_database.sql
+++ V0_1__Initialize_database.sql
@@ -2,20 +2,20 @@ create database prog3_td2;
 \c prog3_td2;
 
 create table team(
-    id_team int serial primary key,
+    id_team  serial primary key,
     team_name varchar
-)
+);
 create table player(
-    id_player int serial primary key,
+    id_player serial primary key,
     player_name varchar,
     numbers int,
     id_team int references team (id_team)
-)
+);
 create table sponsor(
-    id_sponsor int serial primary key,
+    id_sponsor  serial primary key,
     name_sponsor varchar
-)
+);
 create table have(
     id_team int references team (id_team),
     id_sponsor int references sponsor (id_sponsor) 
-)
+);
